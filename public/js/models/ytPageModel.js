bc93baa541bf775e36b3170518283e66184711ab
Emanuele <emanuele.sardini@gmail.com>
2019-06-01 19:41:02 +0200

    youtube page

--- public/js/models/ytPageModel.js
+++ public/js/models/ytPageModel.js
@@ -2,7 +2,10 @@ var YtPageModel = function(params, status){
 	var self = this;
 	console.log("YtPageModel");
 	self.page = ko.observable("YtPageModel");
+	self.ytUrlHistory = ko.observableArray([]);
 
 	self.params = params;
+
+	self.params.socket.emit('youtube history', '');
     
 }
